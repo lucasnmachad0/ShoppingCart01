f5fb7fc4c17ba165cade2737c97f9bffa50d9915
ALUNO <ALUNO@10.85.198.9>
2024-06-04 16:20:38 -0300

    Commit project ShoppingCart01

---
+++ src/ShoppingCart01.java
@@ -0,0 +1,9 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        String custName = "Alex";      //nome da pessoa que quer comprar o produto
+        String itemDesc = "Camisa";    //string que descreve o produto, no caso a camisa
+        String message = custName + " Quer comprar uma " + itemDesc; //string que faz o nome do comprador (alex) mais a mensagem e no final o item.
+        //saida
+        System.out.println(message); //codigo para imprimir a mensagem final.
+    }
+}
